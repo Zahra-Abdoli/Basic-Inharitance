5ad69b4949d22829250ae76badf2e1acecceb160
zahra1993a <33681568+zahra1993a@users.noreply.github.com>
2020-05-20 21:34:59 +0200

    Add project files.

---
+++ BasicInharitance/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasicInharitance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
