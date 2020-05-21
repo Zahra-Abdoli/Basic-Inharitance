ff49e411236d9337b9f5575fd6fe1261a7186716
zahra1993a <33681568+zahra1993a@users.noreply.github.com>
2020-05-21 13:59:27 +0200

    exersize 4 added

---
+++ RandomList/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace RandomList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RandomClass list = new RandomClass();
+            list.Add("ali");
+            list.Add("hassan");
+            list.Add("nima");
+            list.Add("mohamad");
+            list.Add("arman");
+
+            Console.WriteLine(list.RandomString());
+
+        }
+    }
+}
---
+++ RandomList/RandomClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RandomList
+{
+    class RandomClass:List<string>
+    {
+        public string RandomString() 
+        {
+            Random rand = new Random();
+            int index = rand.Next(this.Count);
+            
+           string item= this[index];
+            this.Remove(item);
+            return item;
+
+
+
+        }
+    }
+}
