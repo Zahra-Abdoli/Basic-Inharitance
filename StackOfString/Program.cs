cadd71a4beed103e895d647811f65a8d40cb12fe
zahra1993a <33681568+zahra1993a@users.noreply.github.com>
2020-05-21 15:32:10 +0200

    exersise 5 added

---
+++ StackOfString/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StackOfString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StackOfString stack = new StackOfString();
+            stack.Push("aff");
+            stack.Push("aff");
+            stack.Push("aff");
+            stack.Push("aff");
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Peek());
+            Console.WriteLine(stack.IsEmpty());
+        }
+    }
+}
---
+++ StackOfString/StackOfString.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackOfString
+{
+    class StackOfString
+    {
+        private List<string> data = new List<string>();
+
+        public List<string> Data
+        {
+            get { return data; }
+             private set { data = value; }
+        }
+        public void Push(string item) => data.Add(item);  
+
+        public string Pop() {
+            string item = data[data.Count-1];
+            data.Remove(item);
+            return item;
+        }
+        public string Peek() { 
+            string item = data[data.Count-1];
+            return item;
+        }
+        public bool IsEmpty() { 
+            if (data.Count == 0) return true;
+            else return false;
+        }
+
+
+
+    }
+}
